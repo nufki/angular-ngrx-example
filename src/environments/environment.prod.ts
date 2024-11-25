e349169c8e6a1fe9bca7e39b676315313f62b5e4
nuferkei <stephankei.nufer@raiffeisen.ch>
2024-11-25 15:54:05 +0100

    adopted the api link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  taskAPIUrl: 'https://lo5fyttuo0.execute-api.eu-west-1.amazonaws.com/prod/tasks',
+  taskAPIUrl: 'https://jenrk2iid3.execute-api.eu-west-1.amazonaws.com/prod/tasks',
 };
